6ca0f7ec745348cbbfccf702383314fbfd896297
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2020-05-30 14:11:24 +0100

    Refactor fortune.sh

--- fortune.sh
+++ fortune.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
-# to update this link, go to Fortune 500 list and check requests that load the info about companies
-curl -v -O franchises.json 'https://content.fortune.com/wp-json/irving/v1/data/franchise-search-results?list_id=2814606'
+# to update this URL, go to Fortune 500 list and check requests that load the info about companies
+URL='https://content.fortune.com/wp-json/irving/v1/data/franchise-search-results?list_id=2814606'
+FRANCHISES='fortune-franchises.json'
+PERMALINKS='fortune-permalinks.txt'
+WEBSITES='fortune-websites.txt'
+OUTFILE='fortune.txt'
 
-grep -Po '"permalink":"[^"]+"' franchises.json | sort -u | cut -d: -f2- | tr -d '"\\' | tee permalinks.txt
+curl -o $FRANCHISES $URL
+
+grep -Po '"permalink":"[^"]+"' $FRANCHISES | sort -u | cut -d: -f2- | tr -d '"\\' | tee $PERMALINKS
 
 while read perma; do
-    curl -s $perma;
-done < permalinks.txt \
+    curl -s "$perma";
+done < $PERMALINKS \
     | grep --line-buffered -Po '"website":"[^"]+"' \
     | tee /dev/tty | uniq | cut -d: -f2- | tr -d '"\\' \
-    > websites.txt
+    > $WEBSITES
 
-cat websites.txt | rev | cut -d. -f1,2 | rev | tee fortune1000.txt
+cat $WEBSITES | sed -e 's/.*:\/\///g' | tee $OUTFILE
