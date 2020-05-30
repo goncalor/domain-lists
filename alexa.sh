19935ebe7c58914d439d390f913b3339407fe2c2
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2020-05-30 19:03:56 +0100

    Pass TLDs as arguments to alexa.sh

--- alexa.sh
+++ alexa.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-curl http://s3.amazonaws.com/alexa-static/top-1m.csv.zip | zcat - | cut -d, -f2 > alexa.txt
+DEFAULT=(com pt)
+TLDS=(${@:-${DEFAULT[@]}})  # use arguments or default
+OUTFILE='alexa.txt'
 
-for tld in com pt;
+if [ ! -f "$OUTFILE" ]; then
+    curl http://s3.amazonaws.com/alexa-static/top-1m.csv.zip | zcat - | cut -d, -f2 > "$OUTFILE"
+fi
+
+for tld in "${TLDS[@]}";
 do
     echo .$tld
-    grep "\\.$tld$" alexa.txt | tee "alexa-$tld.txt" | wc -l
+    grep "\\.$tld$" "$OUTFILE" | tee "alexa-$tld.txt" | wc -l
 done
