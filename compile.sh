8308b57070f74e94b035c07dea5437ee17d6579e
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2020-05-31 00:45:40 +0100

    Refactor compile.sh. Create .tar.gz

--- compile.sh
+++ compile.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
 ALEXA_TOP=500
+ALEXA_FILES="alexa.txt alexa-*.txt"
+FORTUNE_FILES="fortune.txt"
+MOZ_FILES="moz.txt"
+W3STATS_FILES="w3stats-*.txt"
+OUTFILE="domains.txt"
 
-cat <(head -q -n$ALEXA_TOP alexa.txt alexa-*.txt) \
-    <(cat fortune.txt | rev | cut -d. -f1,2 | rev) \
+cat <(head -q -n$ALEXA_TOP $ALEXA_FILES) \
+    <(cat $FORTUNE_FILES | rev | cut -d. -f1,2 | rev) \
     moz.txt \
-    w3stats-*.txt \
+    $W3STATS_FILES \
     | sort -u \
-    | tee domains.txt \
+    | tee "$OUTFILE" \
     | wc -l
+
+tar cvaf domains.tar.gz $OUTFILE $ALEXA_FILES $FORTUNE_FILES $MOZ_FILES $W3STATS_FILES
