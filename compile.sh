418d426e497d2846ae1ad63c4c5335b392b1d250
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2020-08-15 23:57:55 +0100

    Add telephone.txt to compile.sh

--- compile.sh
+++ compile.sh
@@ -4,15 +4,17 @@ ALEXA_TOP=500
 ALEXA_FILES="alexa.txt alexa-*.txt"
 FORTUNE_FILES="fortune.txt"
 MOZ_FILES="moz.txt"
+TELEPHONE_FILES="telephone.txt"
 W3STATS_FILES="w3stats-*.txt"
 OUTFILE="domains.txt"
 
 cat <(head -q -n$ALEXA_TOP $ALEXA_FILES) \
     <(cat $FORTUNE_FILES | rev | cut -d. -f1,2 | rev) \
-    moz.txt \
+    $MOZ_FILES \
+    <(cat $TELEPHONE_FILES | sed -e 's/^www\.//') \
     $W3STATS_FILES \
     | sort -u \
     | tee "$OUTFILE" \
     | wc -l
 
-tar cvaf domains.tar.gz $OUTFILE $ALEXA_FILES $FORTUNE_FILES $MOZ_FILES $W3STATS_FILES
+tar cvaf domains.tar.gz $OUTFILE $ALEXA_FILES $FORTUNE_FILES $MOZ_FILES $TELEPHONE_FILES $W3STATS_FILES
